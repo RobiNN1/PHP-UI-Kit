433b9aacddaa4e1fc64a45507d7d2570c07559e2
RobiNN <kelcakrobo@gmail.com>
2022-03-22 23:27:32 +0100

    Fix config

--- examples/twig/index.php
+++ examples/twig/index.php
@@ -9,11 +9,13 @@ require_once __DIR__.'/../../vendor/autoload.php';
  * @return RobiNN\UiKit\UiKit
  */
 function get_ui(): RobiNN\UiKit\UiKit {
-    return RobiNN\UiKit\UiKit::getInstance([
+    $config = new RobiNN\UiKit\Config([
         'cache'     => __DIR__.'/../cache',
         'debug'     => true,
         'framework' => isset($_GET['sm']) ? 'semanticui2' : 'bootstrap5', // for development purposes
     ]);
+
+    return RobiNN\UiKit\UiKit::getInstance($config);
 }
 
 get_ui()->setPath(__DIR__.'/templates/');
