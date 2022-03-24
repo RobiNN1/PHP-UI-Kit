934f5459dcab197786fd8a3799c651730f810e52
RobiNN <kelcakrobo@gmail.com>
2022-03-24 20:30:47 +0100

    Move tpl functions into extension

---
+++ src/TplEngines/TwigUiKitExtension.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of Uikit.
+ *
+ * Copyright (c) Róbert Kelčák (https://kelcak.com/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RobiNN\UiKit\TplEngines;
+
+use RobiNN\UiKit\UiKit;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class TwigUiKitExtension extends AbstractExtension {
+    public function __construct(private UiKit $uikit) {
+    }
+
+    public function getFunctions() {
+        $functions = [];
+
+        foreach ($this->uikit->tplFunctions() as $function => $callback) {
+            $functions[] = new TwigFunction($function, $callback, ['is_safe' => ['html']]);
+        }
+
+        return $functions;
+    }
+
+    public function getFilters() {
+        $filters = [];
+
+        foreach ($this->uikit->tplFilters() as $filter => $callback) {
+            $filters[] = new TwigFilter($filter, $callback);
+        }
+
+        return $filters;
+    }
+}
