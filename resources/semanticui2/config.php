11c998548ce9c778929413987f36de420245c835
RobiNN <kelcakrobo@gmail.com>
2022-04-15 20:35:03 +0200

    Fix grid

--- resources/semanticui2/config.php
+++ resources/semanticui2/config.php
@@ -11,17 +11,17 @@ return [
         'css' => ['https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.8/dist/semantic.min.css'],
         'js'  => ['https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.8/dist/semantic.min.js'],
     ],
-    'grid_func'    => function ($col_sizes): string {
-        if ($col_sizes === 'auto') {
-            return '';
-        }
-
-        $sizes = ['tablet', 'computer', 'large screen'];
+    'grid_func'    => function (array $col_sizes): string {
+        $sizes = ['', 'tablet', 'computer', 'large screen'];
         $columns = [];
 
         foreach ($col_sizes as $index => $value) {
+            if ($value === 'auto') {
+                return '';
+            }
+
             if (is_array($value) && !empty($value['semanticui2'])) {
-                return $value['semanticui2'];
+                return $value['semanticui2'] === 'auto' ? '' : $value['semanticui2'];
             }
 
             if (!is_array($value)) {
@@ -34,17 +34,13 @@ return [
             }
         }
 
-        if (!empty($columns)) {
-            return implode(' ', array_map(function ($size, $column) {
-                $words = [
-                    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-                    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
-                ];
-                return $words[$column].' wide '.$size;
-            }, array_keys($columns), array_values($columns)));
-        }
-
-        return '';
+        return implode(' ', array_map(function ($size, $column): string {
+            $words = [
+                '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+                'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+            ];
+            return $words[$column].' wide '.$size;
+        }, array_keys($columns), array_values($columns)));
     },
     'input'        => [
         'sizes'      => [
