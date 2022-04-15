ad7f44d14ca1aac0219363409f1da223f95f4b2d
RobiNN <kelcakrobo@gmail.com>
2022-04-15 19:43:38 +0200

    Add .phpstorm.meta.php

---
+++ .phpstorm.meta.php
@@ -0,0 +1,30 @@
+<?php
+namespace PHPSTORM_META {
+    override(\RobiNN\UiKit\Components::__get(0), map([
+        // Layout
+        'layout'       => \RobiNN\UiKit\Components\Layout\Layout::class,
+        'container'    => \RobiNN\UiKit\Components\Layout\Container::class,
+        'row'          => \RobiNN\UiKit\Components\Layout\Row::class,
+        'grid'         => \RobiNN\UiKit\Components\Layout\Grid::class,
+        // Form
+        'form'         => \RobiNN\UiKit\Components\Form\Form::class,
+        'input'        => \RobiNN\UiKit\Components\Form\Input::class,
+        'select'       => \RobiNN\UiKit\Components\Form\Select::class,
+        // Components
+        'accordion'    => \RobiNN\UiKit\Components\Accordion::class,
+        'alert'        => \RobiNN\UiKit\Components\Alert::class,
+        'badge'        => \RobiNN\UiKit\Components\Badge::class,
+        'breadcrumbs'  => \RobiNN\UiKit\Components\Breadcrumbs::class,
+        'button'       => \RobiNN\UiKit\Components\Button::class,
+        'button_group' => \RobiNN\UiKit\Components\ButtonGroup::class,
+        'card'         => \RobiNN\UiKit\Components\Card::class,
+        'carousel'     => \RobiNN\UiKit\Components\Carousel::class,
+        'dropdown'     => \RobiNN\UiKit\Components\Dropdown::class,
+        'list_group'   => \RobiNN\UiKit\Components\ListGroup::class,
+        'menu'         => \RobiNN\UiKit\Components\Menu::class,
+        'modal'        => \RobiNN\UiKit\Components\Modal::class,
+        'pagination'   => \RobiNN\UiKit\Components\Pagination::class,
+        'progress'     => \RobiNN\UiKit\Components\Progress::class,
+        'tabs'         => \RobiNN\UiKit\Components\Tabs::class,
+    ]));
+}
