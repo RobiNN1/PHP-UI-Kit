1dfecf627071e2d5ea08611e6f14fd640ef50352
RobiNN <kelcakrobo@gmail.com>
2022-05-22 23:46:30 +0200

    Update LayoutTest.php

--- tests/Components/Layout/LayoutTest.php
+++ tests/Components/Layout/LayoutTest.php
@@ -21,12 +21,12 @@ final class LayoutTest extends ComponentTestCase {
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>UI Kit</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
     </head>
     <body>
         test
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>';
 
