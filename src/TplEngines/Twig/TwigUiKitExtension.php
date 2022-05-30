604e0ae02c744fb6b2a34d7451b62455b054ef57
RobiNN <kelcakrobo@gmail.com>
2022-05-30 20:11:09 +0200

    Small fixes

--- src/TplEngines/Twig/TwigUiKitExtension.php
+++ src/TplEngines/Twig/TwigUiKitExtension.php
@@ -12,6 +12,8 @@ declare(strict_types=1);
 
 namespace RobiNN\UiKit\TplEngines\Twig;
 
+use RobiNN\UiKit\AddTo;
+use RobiNN\UiKit\Misc;
 use RobiNN\UiKit\UiKit;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
@@ -24,20 +26,29 @@ class TwigUiKitExtension extends AbstractExtension {
     public function getFunctions(): array {
         $functions = [];
 
-        foreach ($this->uikit->tplFunctions() as $function => $callback) {
-            $functions[] = new TwigFunction($function, $callback, ['is_safe' => ['html']]);
+        $is_safe = ['is_safe' => ['html']];
+
+        foreach ($this->uikit->allComponents() as $name => $component) {
+            if ((bool) $component['open_close'] === true) {
+                $functions[] = new TwigFunction($name.'_open', [$this->uikit->getComponent($name), 'open'], $is_safe);
+                $functions[] = new TwigFunction($name.'_close', [$this->uikit->getComponent($name), 'close'], $is_safe);
+            }
+
+            $functions[] = new TwigFunction($name, [$this->uikit->getComponent($name), 'render'], $is_safe);
         }
+
+        $functions[] = new TwigFunction('add_to_head', [AddTo::class, 'head'], $is_safe);
+        $functions[] = new TwigFunction('add_to_footer', [AddTo::class, 'footer'], $is_safe);
+        $functions[] = new TwigFunction('add_to_js', [AddTo::class, 'js'], $is_safe);
+        $functions[] = new TwigFunction('add_to_jquery', [AddTo::class, 'jQuery'], $is_safe);
+        $functions[] = new TwigFunction('add_to_css', [AddTo::class, 'css'], $is_safe);
 
         return $functions;
     }
 
     public function getFilters(): array {
-        $filters = [];
-
-        foreach ($this->uikit->tplFilters() as $filter => $callback) {
-            $filters[] = new TwigFilter($filter, $callback);
-        }
-
-        return $filters;
+        return [
+            new TwigFilter('space', [Misc::class, 'space']),
+        ];
     }
 }
