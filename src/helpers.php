f42c4bcc1eb4218935140479201267dfc66a96e6
Róbert Kelčák <kelcakrobo@gmail.com>
2022-10-03 01:31:28 +0200

    update

--- src/helpers.php
+++ src/helpers.php
@@ -411,3 +411,41 @@ if (!function_exists('tabs')) {
         return get_ui()->tabs->render($id, $items, $options);
     }
 }
+
+if (!function_exists('str_contains')) {
+    /**
+     * PHP 8 Polyfill.
+     *
+     * @param string $haystack
+     * @param string $needle
+     *
+     * @return bool
+     */
+    function str_contains(string $haystack, string $needle): bool {
+        return '' === $needle || false !== strpos($haystack, $needle);
+    }
+}
+
+if (!function_exists('str_ends_with')) {
+    /**
+     * PHP 8 Polyfill.
+     *
+     * @param string $haystack
+     * @param string $needle
+     *
+     * @return bool
+     */
+    function str_ends_with(string $haystack, string $needle): bool {
+        if ('' === $needle || $needle === $haystack) {
+            return true;
+        }
+
+        if ('' === $haystack) {
+            return false;
+        }
+
+        $needleLength = strlen($needle);
+
+        return $needleLength <= strlen($haystack) && 0 === substr_compare($haystack, $needle, -$needleLength);
+    }
+}
